a8bd35bec1b4a78026342c796653f2e02e9468cf
Claus Schlereth <97148663+LeshanDaFo@users.noreply.github.com>
2025-06-15 10:55:54 +0800

    adjust ApS.CRT Image
    VICE r45698 can recognize now different module version.
    This is handled by setting bit $1A in the CRT-header to $01 for gray module version, a 0 indicates the black module version

--- makecrt.sh
+++ makecrt.sh
@@ -7,3 +7,4 @@ BUILD="/Volumes/Common/GitHub/C64-COMAL-2.01/build"
 
 $VICE/bin/cartconv -i $BUILD/comal80_Com.bin -t comal -n "COMAL-80" -o $COMAL/Comal80_Com.crt
 $VICE/bin/cartconv -i $BUILD/comal80_ApS.bin -t comal -n "Comal 80" -o $COMAL/Comal80_ApS.crt
+printf '\x01' | dd of=$COMAL/Comal80_ApS.crt bs=1 seek=26 count=1 conv=notrunc
